d9cf63afa85807ec35a8ea1cf207327b5e7ad9aa
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-08-05 12:10:00 -0300

    fix code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,12 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-    
     <main className="flex min-h-screen items-center justify-between">
+     
     <nav className="absolute right-2 top-2">
-    <ModeToggle />
+     <ModeToggle />
     </nav>
 
     <aside className="h-screen hidden lg:block">
@@ -19,42 +18,41 @@ export default function Home() {
     </aside>
 
     <section className="flex flex-col items-center gap-7">
-    <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48"/>
-    </svg>
-    
-    
-     <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
-
-     <Tabs defaultValue="entrar" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="entrar">entrar</TabsTrigger>
-        <TabsTrigger value="registrar">registrar</TabsTrigger>
-      </TabsList>
+      <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48"/>
+      </svg>
       
-      <TabsContent value="entrar">
-        <form action="" className="space-y-8 p-4">
-          <div>
-          <Label htmlFor="email">e-mail</Label>
-          <Input id="email" />
-          </div>
-          <div>
-      <Label htmlFor="senha">senha</Label>
-      <Input type="password" id="senha"/>
-      </div>
-      </TabsContent>
-      </form>
-      </TabsContent>
+      <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
+
+      <Tabs defaultValue="entrar" className="w-[400px]">
         
-        <TabsContent value="rengistrar">
-          <p>criar contas</p>
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="entrar">entrar</TabsTrigger>
+          <TabsTrigger value="registrar">registrar</TabsTrigger>
+        </TabsList>
+        
+        <TabsContent value="entrar">
+          <form action="" className="space-y-8 p-4">
+            <div>
+              <Label htmlFor="email">e-mail</Label>
+              <Input id="email" />
+            </div>
+            <div>
+              <Label htmlFor="senha">senha</Label>
+              <Input type="password" id="senha"/>
+            </div>
+            <Button type="submit">entrar</Button>
+          </form>
         </TabsContent>
-         
-     
-     <Button>entrar</Button>
-     
-     </section>
+        
+        <TabsContent value="registrar">
+          <p>criar conta</p>
+        </TabsContent>
+      </Tabs>
+
+    </section>
+
     
     </main>
   );
-
+}
