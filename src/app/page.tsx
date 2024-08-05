8e3cb0703947da00ff616224610abe4b9856d6fc
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-08-05 11:24:45 -0300

    Configuração do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <nav className="absolute right-2 top-2">
     <ModeToggle />
+    </nav>
      <h1>Sphere</h1>
      <Button>Login</Button>
     </main>
